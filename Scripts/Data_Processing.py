6ae563a2c8c90a9add43e351c562810b5e1e8b08
Mike <mikeanderson0289@gmail.com>
2021-11-15 09:54:16 -0600

    neural network added

--- Scripts/Data_Processing.py
+++ Scripts/Data_Processing.py
@@ -2,11 +2,17 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
+pd.set_option('display.max_columns', None)
+
 
 def DataProcessing(csv_url):
     df = pd.read_csv(csv_url)
     df.columns = [x.title().strip() for x in df.columns]
     df = df.dropna(subset=['S1'])
+
+    drop_cols = ['S1_Large', 'S2_Large', 'S3_Large']
+
+    df['Event'].value_counts()
 
     for index, row in df.iterrows():
         number = str(row['Number'])
@@ -89,5 +95,87 @@ def DataProcessing(csv_url):
                 pit_time += pit_amount
 
         df.loc[df['Trial_ID_2'] == x, 'Pit_Time'] = pit_time / laps
+
+    df = df.drop(columns=[
+    'S1_Large',
+    'S2_Large',
+    'S3_Large',
+    'Trial_Number',
+    'Trial_ID',
+    'Number',
+    'Driver_Number',
+    'Crossing_Finish_Line_In_Pit'
+    ])
+
+    def ConvertToSeconds(x):
+        y = x.total_seconds()
+        return y
+
+    df['S1'] = df['S1'].apply(ConvertToSeconds)
+    df['S2'] = df['S2'].apply(ConvertToSeconds)
+    df['S3'] = df['S3'].apply(ConvertToSeconds)
+    df['Pit_Time'] = df['Pit_Time'].apply(ConvertToSeconds)
+    df['Elapsed'] = df['Elapsed'].apply(ConvertToSeconds)
+
+    df['Time_Minutes'] = [(x.total_seconds() / 60) for x in df['Hour']]
+
+    wdf_train = pd.read_csv('../Data/train_weather.csv')
+    wdf_test = pd.read_csv('../Data/test_weather.csv')
+    wdf_test = wdf_test.rename(columns={'EVENTS': 'EVENT'})
+    wdf_test['RAIN'] = wdf_test['RAIN'].astype(str)
+
+    wdf = pd.concat([wdf_train, wdf_test])
+
+    wdf['TIME_UTC_STR'] = pd.to_datetime(wdf['TIME_UTC_STR'], dayfirst=True)
+    wdf['TIME_UTC_MINUTE'] = [x.minute for x in wdf['TIME_UTC_STR']]
+
+    num_cols = ['AIR_TEMP', 'TRACK_TEMP', 'HUMIDITY', 'PRESSURE', 'WIND_SPEED', 'RAIN']
+    wdf['RAIN'] = [x.replace(',', '.') for x in wdf['RAIN']]
+
+    for col in num_cols:
+        wdf[col] = wdf[col].str.replace(',', '').replace('.', '').astype(float)
+
+    wdf.dtypes
+
+    for index, row in df.iterrows():
+        location = row['Location']
+        event = row['Event']
+        time = row['Time_Minutes']
+
+        try:
+            weather = wdf.loc[(wdf['LOCATION'] == location) & (wdf['EVENT'] == event) & (wdf['TIME_UTC_MINUTE'] <= time) & (time < (wdf['TIME_UTC_MINUTE'] + 1))]
+            weather = weather.iloc[0]
+            df.at[index, 'Air_Temp'] = weather['AIR_TEMP']
+            df.at[index, 'Track_Temp'] = weather['TRACK_TEMP']
+            df.at[index, 'Humidity'] = weather['HUMIDITY']
+            df.at[index, 'Pressure'] = weather['PRESSURE']
+            df.at[index, 'Wind_Speed'] = weather['WIND_SPEED']
+            df.at[index, 'Wind_Direction'] = weather['WIND_DIRECTION']
+            df.at[index, 'Rain'] = weather['RAIN']
+        except IndexError:
+            weather = wdf.loc[(wdf['LOCATION'] == location) & (wdf['EVENT'] == event)]
+            if not weather.empty:
+                df.at[index, 'Air_Temp'] = weather['AIR_TEMP'].mean()
+                df.at[index, 'Track_Temp'] = weather['TRACK_TEMP'].mean()
+                df.at[index, 'Humidity'] = weather['HUMIDITY'].mean()
+                df.at[index, 'Pressure'] = weather['PRESSURE'].mean()
+                df.at[index, 'Wind_Speed'] = weather['WIND_SPEED'].mean()
+                df.at[index, 'Wind_Direction'] = weather['WIND_DIRECTION'].mean()
+                df.at[index, 'Rain'] = weather['RAIN'].mode()[0]
+            else:
+                weather = wdf.loc[wdf['LOCATION'] == location]
+                df.at[index, 'Air_Temp'] = weather['AIR_TEMP'].mean()
+                df.at[index, 'Track_Temp'] = weather['TRACK_TEMP'].mean()
+                df.at[index, 'Humidity'] = weather['HUMIDITY'].mean()
+                df.at[index, 'Pressure'] = weather['PRESSURE'].mean()
+                df.at[index, 'Wind_Speed'] = weather['WIND_SPEED'].mean()
+                df.at[index, 'Wind_Direction'] = weather['WIND_DIRECTION'].mean()
+                df.at[index, 'Rain'] = weather['RAIN'].mode()[0]
+
+
+    df['Power'] = df['Power'].fillna(df['Power'].mode()[0])
+    df['Kph'] = df['Kph'].fillna(df['Kph'].mean())
+    df = df.drop(columns=['Group', 'Hour', 'Trial_ID_2', 'Time_Minutes'])
+    df['Lap_Time'] = df['Lap_Time'].astype(float)
 
     return df
