4073b780574d95497ad701056711f008de5d0fa0
Mike <mikeanderson0289@gmail.com>
2021-11-19 16:01:02 -0600

    restructured data processing

--- Scripts/Data_Processing.py
+++ Scripts/Data_Processing.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
-pd.set_option('display.max_columns', None)
 
 
 def DataProcessing(csv_url):
@@ -126,16 +125,81 @@ def DataProcessing(csv_url):
 
     wdf = pd.concat([wdf_train, wdf_test])
 
+    #split into location due to different number formats
+    train_weather_l1 = wdf[wdf['LOCATION'].isin(['Location 1','Location 2','Location 3','Location 4'])]
+    train_weather_l1['AIR_TEMP'] = train_weather_l1['AIR_TEMP'] .str.replace(',','.')
+    train_weather_l1['AIR_TEMP'] = pd.to_numeric(train_weather_l1['AIR_TEMP'])
+    train_weather_l1['TRACK_TEMP'] = train_weather_l1['TRACK_TEMP'] .str.replace(',','.')
+    train_weather_l1['TRACK_TEMP'] = pd.to_numeric(train_weather_l1['TRACK_TEMP'])
+    train_weather_l1['HUMIDITY'] = train_weather_l1['HUMIDITY'] .str.replace(',','.')
+    train_weather_l1['HUMIDITY'] = pd.to_numeric(train_weather_l1['HUMIDITY'])
+    train_weather_l1['PRESSURE'] = train_weather_l1['PRESSURE'] .str.replace(',','.')
+    train_weather_l1['PRESSURE'] = pd.to_numeric(train_weather_l1['PRESSURE'])
+    train_weather_l1['WIND_SPEED'] = train_weather_l1['WIND_SPEED'] .str.replace(',','.')
+    train_weather_l1['WIND_SPEED'] = pd.to_numeric(train_weather_l1['WIND_SPEED'])
+    train_weather_l1['RAIN'] = train_weather_l1['RAIN'].str.replace(',', '.')
+    train_weather_l1['RAIN'] = pd.to_numeric(train_weather_l1['RAIN'])
+
+
+
+    train_weather_l2 = wdf[wdf['LOCATION'].isin(['Location 5','Location 6','Location 7', 'Location 8'])]
+    train_weather_l2['AIR_TEMP'] = train_weather_l2['AIR_TEMP'] .str.replace(',','')
+    train_weather_l2['AIR_TEMP'] = pd.to_numeric(train_weather_l2['AIR_TEMP'], errors='coerce')
+    conditions = [
+        (train_weather_l2['AIR_TEMP'] > 100)  & (train_weather_l2['AIR_TEMP'] < 1000),
+        (train_weather_l2['AIR_TEMP'] > 1000) & (train_weather_l2['AIR_TEMP'] < 10000),
+        (train_weather_l2['AIR_TEMP'] > 10000) & (train_weather_l2['AIR_TEMP'] < 100000),
+        (train_weather_l2['AIR_TEMP'] > 100000)]
+    choices = [train_weather_l2['AIR_TEMP']/10,train_weather_l2['AIR_TEMP']/100,
+               train_weather_l2['AIR_TEMP']/1000,train_weather_l2['AIR_TEMP']/10000]
+    train_weather_l2['AIR_TEMP'] = np.select(conditions, choices, default=20)
+
+    train_weather_l2['TRACK_TEMP'] = train_weather_l2['TRACK_TEMP'] .str.replace(',','.')
+    train_weather_l2['TRACK_TEMP'] = pd.to_numeric(train_weather_l2['TRACK_TEMP'], errors='coerce')
+
+    train_weather_l2['HUMIDITY'] = train_weather_l2['HUMIDITY'] .str.replace(',','.')
+    train_weather_l2['HUMIDITY'] = pd.to_numeric(train_weather_l2['HUMIDITY'], errors='coerce')
+
+
+
+    train_weather_l2['PRESSURE'] = train_weather_l2['PRESSURE'] .str.replace(',','')
+    train_weather_l2['PRESSURE'] = pd.to_numeric(train_weather_l2['PRESSURE'], errors='coerce')
+    conditions = [
+        (train_weather_l2['PRESSURE'] > 10000) & (train_weather_l2['PRESSURE'] < 20000),
+        (train_weather_l2['PRESSURE'] > 20000) & (train_weather_l2['PRESSURE'] < 200000),
+        (train_weather_l2['PRESSURE'] > 200000)]
+
+    choices = [train_weather_l2['PRESSURE']/10,
+               train_weather_l2['PRESSURE']/100,
+               train_weather_l2['PRESSURE']/1000]
+    train_weather_l2['PRESSURE'] = np.select(conditions, choices, default=1000)
+
+
+    train_weather_l2['WIND_SPEED'] = train_weather_l2['WIND_SPEED'] .str.replace(',','')
+    train_weather_l2['WIND_SPEED'] = pd.to_numeric(train_weather_l2['WIND_SPEED'], errors='coerce')
+    conditions = [
+        (train_weather_l2['WIND_SPEED'] > 10) & (train_weather_l2['WIND_SPEED'] < 100),
+        (train_weather_l2['WIND_SPEED'] > 100) & (train_weather_l2['WIND_SPEED'] < 1000),
+        (train_weather_l2['WIND_SPEED'] > 1000) & (train_weather_l2['WIND_SPEED'] < 10000),
+        (train_weather_l2['WIND_SPEED'] > 10000) & (train_weather_l2['WIND_SPEED'] < 100000),
+        (train_weather_l2['WIND_SPEED'] > 100000)]
+
+    choices = [train_weather_l2['WIND_SPEED']/10,
+               train_weather_l2['WIND_SPEED']/100,
+               train_weather_l2['WIND_SPEED']/1000,
+               train_weather_l2['WIND_SPEED']/10000,
+               train_weather_l2['WIND_SPEED']/100000,
+               ]
+    train_weather_l2['WIND_SPEED'] = np.select(conditions, choices, default=1)
+    train_weather_l2['RAIN'] = train_weather_l2['RAIN'].str.replace(',', '.')
+    train_weather_l2['RAIN'] = pd.to_numeric(train_weather_l2['RAIN'])
+
+    wdf = pd.concat([train_weather_l1,train_weather_l2])
+
     wdf['TIME_UTC_STR'] = pd.to_datetime(wdf['TIME_UTC_STR'], dayfirst=True)
     wdf['TIME_UTC_MINUTE'] = [x.minute for x in wdf['TIME_UTC_STR']]
 
     num_cols = ['AIR_TEMP', 'TRACK_TEMP', 'HUMIDITY', 'PRESSURE', 'WIND_SPEED', 'RAIN']
-    wdf['RAIN'] = [x.replace(',', '.') for x in wdf['RAIN']]
-
-    for col in num_cols:
-        wdf[col] = wdf[col].str.replace(',', '').replace('.', '').astype(float)
-
-    wdf.dtypes
 
     for index, row in df.iterrows():
         location = row['Location']
@@ -176,6 +240,5 @@ def DataProcessing(csv_url):
     df['Power'] = df['Power'].fillna(df['Power'].mode()[0])
     df['Kph'] = df['Kph'].fillna(df['Kph'].mean())
     df = df.drop(columns=['Group', 'Hour', 'Trial_ID_2', 'Time_Minutes'])
-    df['Lap_Time'] = df['Lap_Time'].astype(float)
 
     return df
